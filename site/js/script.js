ceb50341b9a6c9bb13810d80598d83f1e3e3d396
VISHU <VISHU>
2016-10-24 15:31:59 +0530

    College Project

--- site/js/script.js
+++ site/js/script.js
@@ -1,15 +1,47 @@
 
-$("#student").hide();
-$("#faculty").hide();
 $("#studentclick").click(function(){
 	if($("#faculty").is(':visible')) {
 		$("#faculty").hide();
 	}
-    $("#student").show();
+	if($("#student").is(':visible')) {
+		$("#student").hide();
+	}
+	else
+    	$("#student").show();
 });
 $("#facultyclick").click(function(){
 	if($("#student").is(':visible')) {
 		$("#student").hide();
 	}
-    $("#faculty").show();
+	if($("#faculty").is(':visible')) {
+		$("#faculty").hide();
+	}
+	else
+    	$("#faculty").show();
 });
+$(document).ready(function(e) {
+	$("#student").hide();
+	$("#faculty").hide();
+
+	$("#emailfaculty").blur(function() {
+		var sEmail = $("#emailfaculty").val();
+		if ($.trim(sEmail).length == 0) {
+            alert('Please enter valid email address');
+            e.preventDefault();
+        }
+        if (!(validateEmail(sEmail))) {
+            alert('Invalid Email Address');
+            $("#emailfaculty").focus();
+            e.preventDefault();
+        }
+	});
+});
+function validateEmail(sEmail) {
+    var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    if (filter.test(sEmail)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
